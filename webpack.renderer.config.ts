e8afcb7907b1a11c994540e09e29acf4269fb75c
dwclake <clake.dw@gmail.com>
2025-10-27 13:40:55 -0700

    feat: scss + tailwind support failure

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -3,10 +3,39 @@ import type { Configuration } from "webpack";
 import { rules } from "./webpack.rules";
 import { plugins } from "./webpack.plugins";
 
-rules.push({
-    test: /\.css$/,
-    use: [{ loader: "style-loader" }, { loader: "css-loader" }]
-});
+rules.push(
+    {
+        test: /\.(scss|css)$/,
+        use: [
+            { loader: "style-loader" },
+            {
+                loader: "css-loader",
+                options: {
+                    importLoaders: 2,
+                    modules: false
+                }
+            },
+            { loader: "postcss-loader" },
+            { loader: "sass-loader" }
+        ]
+    },{
+        test: /\.module\.(scss|css)$/,
+        use: [
+            { loader: "style-loader" },
+            {
+                loader: "css-loader",
+                options: {
+                    importLoaders: 2,
+                    modules: {
+                        localIdentName: "[name]__[local]___[hash:base64:5]",
+                    },
+                },
+            },
+            { loader: "postcss-loader" },
+            { loader: "sass-loader" },
+        ]
+    }
+);
 
 export const rendererConfig: Configuration = {
     module: {
@@ -14,6 +43,6 @@ export const rendererConfig: Configuration = {
     },
     plugins,
     resolve: {
-        extensions: [".js", ".ts", ".jsx", ".tsx", ".css"]
+        extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".scss", ".sass"]
     }
 }

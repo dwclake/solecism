3f85800e69820a3885b5f9c04a72a08ab0485df5
dwclake <clake.dw@gmail.com>
2025-10-29 13:36:09 -0700

    change: npm -> bun

--- src/Infobar.tsx
+++ src/Infobar.tsx
@@ -10,7 +10,7 @@ import styles from "../styles/Infobar.module.scss";
 export const Infobar = () => {
     return (
         <footer className={styles.container}>
-            <span className={styles.project}>{"oddessey"}</span>
+            <span className={styles.project}>{"odyssey"}</span>
             <span className={styles.filename}>{"scene one"}</span>
             <span className={styles.wordcount}>word count: {12}</span>
             <span className={styles.saved}>last saved: {"now" || "12 minutes ago"}</span>

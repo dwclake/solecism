f297c3d84700f581e017a781543859063eca9b70
dwclake <clake.dw@gmail.com>
2025-12-01 20:35:53 -0800

    working but messy

--- src/bun/index.ts
+++ src/bun/index.ts
@@ -1,9 +1,13 @@
 import { BrowserWindow } from "electrobun/bun";
 
+const isDev = !!process.env.ELECTROBUN_SVELTE_DEV;
+const devUrl = process.env.ELECTROBUN_SVELTE_URL || "http://localhost:5173";
+const url = isDev ? devUrl : "views://mainview/index.html";
+
 // Create the main application window
 const mainWindow = new BrowserWindow({
-  title: "Hello Electrobun!",
-  url: "views://mainview/index.html",
+  title: "Solecism",
+  url,
   frame: {
     width: 800,
     height: 800,
@@ -12,4 +16,9 @@ const mainWindow = new BrowserWindow({
   },
 });
 
-console.log("Hello Electrobun app started!");
+// Close app when window is closed
+mainWindow.on("closed", () => {
+  process.exit(0);
+});
+
+console.log("Hello Electrobun app started!", { isDev, url });

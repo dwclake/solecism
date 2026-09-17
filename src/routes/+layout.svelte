87d54e7526330d5e40dbb55891f084390eb11bf4
dwclake <clake.dw@gmail.com>
2026-09-16 21:01:45 -0700

    initial homepage mockup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import Navbar from '#lib/components/Navbar.svelte';
-	import Sidebar from '#lib/components/Sidebar.svelte';
 
 	import './layout.css';
 	import favicon from '#lib/assets/favicon.svg';
@@ -10,14 +9,14 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<div class="flex h-screen">
+<div class="grain-bg z-0 flex h-svh w-svw text-mirage-900 dark:bg-mirage-100 dark:text-mirage-300">
 	<header
-		class="m-4 mr-2 h-min w-min shrink-0 overflow-y-auto rounded-md border-2 bg-mirage-200 shadow-mirage-950 dark:shadow-mirage-200"
+		class="z-1 m-4 mr-2 h-min w-min shrink-0 overflow-y-auto rounded-md border-2 bg-mirage-200 shadow-mirage-950 dark:shadow-mirage-200"
 	>
 		<Navbar />
 	</header>
 	<main
-		class="m-4 ml-2 w-4/5 flex-1 overflow-y-auto rounded-md border-2 bg-mirage-200 shadow-mirage-950 dark:shadow-mirage-200"
+		class="z-1 m-4 ml-2 w-4/5 flex-1 overflow-y-auto rounded-md border-2 bg-mirage-200 shadow-mirage-950 dark:shadow-mirage-200"
 	>
 		{@render children()}
 	</main>

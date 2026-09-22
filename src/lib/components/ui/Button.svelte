190ece02bdc26486f84d6745486544b3a4db7b7b
dwclake <clake.dw@gmail.com>
2026-09-21 17:22:52 -0700

    swapped to vanilla css

--- src/lib/components/ui/Button.svelte
+++ src/lib/components/ui/Button.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	let { children } = $props();
 </script>
 
-<button></button>
+<button class="btn">
+	{@render children()}
+</button>
 
 <style>
+	.btn {
+		border: 2px solid currentColor;
+	}
 </style>
